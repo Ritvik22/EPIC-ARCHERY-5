28893190121759b5e6b7d1fd00a3e8bce4ef3d53
Ritvik22 <85530828+Ritvik22@users.noreply.github.com>
2021-08-01 14:32:11 -0400

    Add files via upload

---
+++ Project_Template_27-main 2/js/Player.js
@@ -0,0 +1,55 @@
+class Player {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+
+
+    World.add(world, this.body);
+
+    this.life1 = "green";
+    this.life2 = "green";
+    this.life3 = "green";
+
+
+  }
+
+ 
+
+ 
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+
+
+Hp () {
+
+  push();
+
+  textSize(25);
+  text("Player HP", 280, 40);
+
+  fill("green");
+
+  rect(180, 50, 70, 30);
+  rect(250, 50, 70, 30);
+  rect(320, 50, 70, 30);
+
+  pop();
+
+}
+
+}
